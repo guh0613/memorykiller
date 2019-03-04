21c0aa37c5569a5afcde6c35793ec4e17f07b6cc
HFDLYS <845388925@qq.com>
2019-03-04 22:41:06 +0800

    手动检测更新搞定

--- app/src/main/java/com/huaji/memorykiller/MainActivity.java
+++ app/src/main/java/com/huaji/memorykiller/MainActivity.java
@@ -39,12 +39,14 @@ public class MainActivity extends Activity
 			.setDeleteHistroyApk(true)     // 检查更新前是否删除本地历史 Apk， 默认为true
 			.setUpdateManagerListener(new UpdateManagerListener() {
 				@Override
-				public void onNoUpdateAvailable() {
+				public void onNoUpdateAvailable() 
+				{
 					//没有更新是回调此方法
 					Log.d("pgyer", "there is no new version");
 				}
 				@Override
-				public void onUpdateAvailable(final AppBean appBean) {
+				public void onUpdateAvailable(final AppBean appBean) 
+				{
 					//有更新回调此方法
 					Log.d("pgyer", "there is new version can update"
 						  + "new versionCode is " + appBean.getVersionCode());
@@ -318,10 +320,9 @@ adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		}
 	}
 	public boolean onCreateOptionsMenu(Menu menu)
-	
-{
-	getMenuInflater().inflate(R.menu.main , menu);
-	return true;
+	{
+		getMenuInflater().inflate(R.menu.main , menu);
+		return true;
 	}
 
 	
@@ -331,35 +332,107 @@ adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			switch (item.getItemId())
 			{
 				case R.id.about:
-					showNormalDialog();
+					showAboutDialog();
 					break;
 				case R.id.updata:
-					Toast.makeText(this , "我不知道怎么更新啊" ,Toast.LENGTH_SHORT).show();
-					finish();
+					new PgyUpdateManager.Builder()
+					.setForced(false)                //设置是否强制提示更新,非自定义回调更新接口此方法有用
+					.setUserCanRetry(true)         //失败后是否提示重新下载，非自定义下载 apk 回调此方法有用
+					.setDeleteHistroyApk(true)     // 检查更新前是否删除本地历史 Apk， 默认为true
+					.setUpdateManagerListener(new UpdateManagerListener() {
+						@Override
+						public void onNoUpdateAvailable()
+						{
+							//没有更新是回调此方法
+							Log.d("pgyer", "there is no new version");
+						}
+						@Override
+						public void onUpdateAvailable(final AppBean appBean)
+						{
+							//有更新回调此方法
+							Log.d("pgyer", "there is new version can update"
+						  	+ "new versionCode is " + appBean.getVersionCode());
+						  	AlertDialog.Builder dialog = new AlertDialog.Builder (MainActivity.this);
+						  	dialog.setTitle("更新提醒");
+						  	dialog.setMessage("应用有新版本，要不要更新？");
+						  	dialog.setCancelable(false);
+						  	dialog.setPositiveButton("好，更新",new DialogInterface.OnClickListener()
+						  		{
+							  		@Override
+							  		public void onClick(DialogInterface dialog,int which)
+							  		{
+								  		PgyUpdateManager.downLoadApk(appBean.getDownloadURL());
+							  		}
+						  		});
+							dialog.setNegativeButton("老子才不更新",new DialogInterface.OnClickListener()
+								{
+									@Override
+									public void onClick(DialogInterface dialog,int which)
+									{
+								
+									}
+								});
+						  dialog.show();
+					//调用以下方法，DownloadFileListener 才有效；
+					//如果完全使用自己的下载方法，不需要设置DownloadFileListener
+					
+				}
+
+				@Override
+				public void checkUpdateFailed(Exception e) {
+					//更新检测失败回调
+					//更新拒绝（应用被下架，过期，不在安装有效期，下载次数用尽）以及无网络情况会调用此接口
+					Log.e("pgyer", "check update failed ", e);
+                }
+            })
+            //注意 ：
+            //下载方法调用 PgyUpdateManager.downLoadApk(appBean.getDownloadURL()); 此回调才有效
+            //此方法是方便用户自己实现下载进度和状态的 UI 提供的回调
+            //想要使用蒲公英的默认下载进度的UI则不设置此方法
+            .setDownloadFileListener(new DownloadFileListener() {   
+                @Override
+                public void downloadFailed() {
+                    //下载失败
+                    Log.e("pgyer", "download apk failed");
+                }
+
+                @Override
+                public void downloadSuccessful(Uri uri) {
+                    Log.e("pgyer", "download apk failed");
+                    // 使用蒲公英提供的安装方法提示用户 安装apk
+                    PgyUpdateManager.installApk(uri);  
+				}
+
+                @Override
+                public void onProgressUpdate(Integer... integers) {
+                    Log.e("pgyer", "update download apk progress" + integers);
+                }})
+			.register();
 					break;
 				default:
 			}
 			return true;
 		}
 	}
-	private void showNormalDialog(){
-    /* @setIcon 设置对话框图标
-     * @setTitle 设置对话框标题
-     * @setMessage 设置对话框消息提示
-     * setXXX方法返回Dialog对象，因此可以链式设置属性
-     */
-   	final AlertDialog.Builder normalDialog = 
-    new AlertDialog.Builder(MainActivity.this);
-    normalDialog.setTitle("没想好");
-    normalDialog.setMessage("我还没想好");
-    normalDialog.setPositiveButton("老子知道了", 
-      new DialogInterface.OnClickListener() {
-      @Override
-      public void onClick(DialogInterface dialog, int which) {
-        //...To-do
-      }
-    });
-    // 显示
-    normalDialog.show();
-  }
+	private void showAboutDialog()
+	{
+    	/* @setIcon 设置对话框图标
+     	* @setTitle 设置对话框标题
+     	* @setMessage 设置对话框消息提示
+     	* setXXX方法返回Dialog对象，因此可以链式设置属性
+    	*/
+   		final AlertDialog.Builder normalDialog = 
+    	new AlertDialog.Builder(MainActivity.this);
+    	normalDialog.setTitle("没想好");
+    	normalDialog.setMessage("我还没想好");
+    	normalDialog.setPositiveButton("老子知道了", 
+      	new DialogInterface.OnClickListener() {
+      		@Override
+      		public void onClick(DialogInterface dialog, int which)
+			{
+        
+      		}
+    	});
+    	normalDialog.show();
+  	}
 }
