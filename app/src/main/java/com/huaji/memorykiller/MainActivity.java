a821606e06d0adb5a6ec79bcd86dcac173bd1cfc
guh0613 <gux1129@126.com>
2019-03-08 23:57:58 +0800

    完善侧拉栏，初步布局完毕

--- app/src/main/java/com/huaji/memorykiller/MainActivity.java
+++ app/src/main/java/com/huaji/memorykiller/MainActivity.java
@@ -13,6 +13,9 @@ import android.net.*;
 import android.view.*;
 import android.support.v4.widget.*;
 import android.support.v4.view.*;
+import android.support.design.widget.*;
+import android.text.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -24,16 +27,57 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 		PgyCrashManager.register(); 
         setContentView(R.layout.main);
-		Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+		android.support.v7.widget.Toolbar toolbar=(android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		mdraw1=(DrawerLayout) findViewById(R.id.draw1);
+		
 		ActionBar actionbar=getSupportActionBar();
-		if (actionbar!=null)
-		{
-			actionbar.setDisplayHomeAsUpEnabled(true);
-			actionbar.setHomeAsUpIndicator(R.drawable.__ic_menu);
-		}
+		ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mdraw1, toolbar,R.string.draw_open,R.string.draw_close);
+																		
+		mDrawerToggle.syncState();//初始化状态
+        mdraw1.setDrawerListener(mDrawerToggle);
+		NavigationView mNavigationView=(NavigationView) findViewById(R.id.nav_view);
+		mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(MenuItem menuItem) {
+					menuItem.setChecked(false);//点击了把它设为选中状态
+					mdraw1.closeDrawers();//关闭抽屉
+					return true;
+					}
+					});
 		checkupdate();
+		final TextInputLayout inputLayout = (TextInputLayout) findViewById(R.id.ET1);
+		inputLayout.setHint("输入要占用的内存：");
+
+        final EditText editText = inputLayout.getEditText();
+        editText.addTextChangedListener(new TextWatcher() {
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after)
+				{
+
+				}
+
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count)
+				{
+					Log.e("TAG", s.length() + "");
+					if (s.length()==0 )
+					{//字符超过5个时，出现EditText提示
+						inputLayout.setError("你起码给个大小吧！");
+						inputLayout.setErrorEnabled(true);
+					}
+					else
+					{
+						inputLayout.setErrorEnabled(false);
+					}
+				}
+
+				@Override
+				public void afterTextChanged(Editable s)
+				{
+
+				}
+			});
     }
 
 	@Override
@@ -54,9 +98,7 @@ public class MainActivity extends AppCompatActivity
 				case R.id.updata:
 					checkupdate();
 					break;
-					case R.id.home:
-						mdraw1.openDrawer(GravityCompat.START);
-						break;
+					
 		}
 		// TODO: Implement this method
 		return true;
